f5f2d84954f68798cf7e5e7147a4b46d73568578
Pierre Laitselart <pierre.laitselart@bigyouth.fr>
2018-10-11 15:06:59 +0200

    API

--- api/server/playground/mongoose-queries.js
+++ api/server/playground/mongoose-queries.js
@@ -1,16 +1,29 @@
+const { ObjectID } = require('mongodb');
+
 const { mongoose } = require('./../mongoose/mongoose');
 const { Todo } = require('./../models/todo');
 
-const id = '5bbf31c6110e780b26bcf0be';
+// const id = '5bbf31c6110e780b26bcf0be';
 
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
-});
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not valid');
+// } 
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo', todo);
-});
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found');
+//     }
+//     console.log('Todo By Id', todo);
+// }).catch((err) => console.log(err));
--- api/server/server.js
+++ api/server/server.js
@@ -1,6 +1,7 @@
 // Library imports
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 // Local imports
 const { mongoose } = require('./mongoose/mongoose');
@@ -12,6 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// POST
 app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
@@ -24,11 +26,28 @@ app.post('/todos', (req, res) => {
     });
 });
 
+// GET ALL
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.status(200).send({todos});
     }, (error) => {
         res.send(400).send(error);
+    });
+});
+
+// GET BY ID
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Id not valid');
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send('Id not found');
+        }
+        res.status(200).send({todo})
+    }).catch((error) => {
+        res.status(400).send(error);
     });
 });
 
