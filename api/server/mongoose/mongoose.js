adfc00ad043a646391b0fccc20a8dacd9119f86f
Pierre Laitselart <pierre.laitselart@bigyouth.fr>
2018-10-11 15:59:34 +0200

    API

--- api/server/mongoose/mongoose.js
+++ api/server/mongoose/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://<root>:<root123>@ds125683.mlab.com:25683/nodejs-api');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
